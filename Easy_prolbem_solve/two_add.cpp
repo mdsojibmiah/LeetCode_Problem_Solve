fc629f8dbbec432f1b4e601c44ae714a558adff5
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-14 23:49:28 +0600

    asdfsdf

---
+++ Easy_prolbem_solve/two_add.cpp
@@ -0,0 +1,32 @@
+/* Author: mdsojibmiah
+   Email: mdsojibmiah.coder@gmail.com
+   Problem Link: https://leetcode.com/problems/add-two-integers/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve(int a, int b) {
+    cout << a + b << endl;
+}
+
+int main() {
+    fast_io;
+    int a, b;
+    cin >> a >> b;
+    solve(a, b);
+    return 0;
+}
