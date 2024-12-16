02f4f747601b579eeecbb50057487e46f968680f
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-16 11:24:46 +0600

    Problem solve div 4

--- Code_forces_Div4/C.cpp
+++ Code_forces_Div4/C.cpp
@@ -1,6 +1,6 @@
 /* Author: Error_404_Coder
    Email: mdsojibmiah.coder@gmail.com
-   Problem Link: 
+   Problem Link: https://codeforces.com/contest/2044/problem/Cs
 */
 
 #include <bits/stdc++.h>
@@ -12,23 +12,22 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 #define F first
 #define S second
-
 const ll INF = 1e18;
 const ll MOD = 1e9 + 7;
 const ld PI = acos(-1.0);
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void solve() {
-    // Your code here
-}
-
 int main() {
     fast_io;
-    int t = 1;
-    cin >> t;
+    int t;cin >> t;
     while (t--) {
-        solve();
+        ll m,a,b,c;cin >> m >> a >> b >> c;
+        ll r1 = min(a,m);
+        ll r2 = min(b,m);
+        ll seat = 2*m-r1-r2;
+        ll p = min(c,seat);
+        cout<< (r1+r2+p) << endl;
     }
     return 0;
 }
