4f0a0feb54e85b18918fb27fe060a7c34db597ca
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-17 19:23:36 +0600

    Adding Bits solve

---
+++ Easy_prolbem_solve/adding_bits.cpp
@@ -0,0 +1,33 @@
+/* Author: Error_404_Coder
+   Email: mdsojibmiah.coder@gmail.com
+   Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/F
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ul unsigned int
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve(ul a,ul b) {
+    ul c = a^b;
+    cout<<c;
+}
+
+int main() {
+    fast_io;
+    ul a, b;cin>>a>>b;
+    solve(a,b);
+    return 0;
+}
