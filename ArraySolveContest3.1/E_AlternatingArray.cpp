c77948da6fdd8d42c5cb0ba5f0a2caa6a0add6f6
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-22 03:04:58 +0600

    upp

--- ArraySolveContest3.1/E_AlternatingArray.cpp
+++ ArraySolveContest3.1/E_AlternatingArray.cpp
@@ -2,7 +2,7 @@
    Email: mdsojibmiah.coder@gmail.com
    Date  : 12 / 22 / 2024
    Time  : 02:55
-   Problem Link: 
+   Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/329103/problem/E
 */
 
 #include <bits/stdc++.h>
@@ -22,16 +22,32 @@ const ld PI = acos(-1.0);
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void solve() {
-    // Your code here
+void solve(int n) {
+    vector<int> a(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    int ops1 = 0, ops2 = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (i % 2 == 0) {
+            if (a[i] < 0) ops1++;
+            if (a[i] > 0) ops2++;
+        } else {
+            if (a[i] > 0) ops1++;
+            if (a[i] < 0) ops2++;
+        }
+    }
+
+    cout << min(ops1, ops2) << endl;
 }
 
 int main() {
     fast_io;
-    int t = 1;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    int n;
+    cin >> n;
+    solve(n);
     return 0;
 }
