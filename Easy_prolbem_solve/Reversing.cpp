65d6ac5fa77cb83d2f0514b822d7b72a319a817c
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-21 20:05:41 +0600

    Reversing solve

---
+++ Easy_prolbem_solve/Reversing.cpp
@@ -0,0 +1,39 @@
+/* Author: Error_404_Coder
+   Email: mdsojibmiah.coder@gmail.com
+   Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/329103/problem/B
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+
+int main() {
+    fast_io;
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    for(int i=0;i<n;i++){
+        if (v[i]==0){
+            reverse(v.begin(),v.begin()+i);
+        }
+    }
+    for (int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
