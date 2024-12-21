5afae827a26739c655ddf9d2ff45f87155afcf17
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-22 02:25:51 +0600

    File create

---
+++ ArraySolveContest3.1/E_AlternationArray.cpp
@@ -0,0 +1,37 @@
+/* Author: Error_404_Coder
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 12 / 22 / 2024
+   Time  : 02:25
+   Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/329103/problem/E
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
