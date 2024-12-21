70832e091a161294ef5b173b840f25414fc4877e
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-22 02:49:03 +0600

    solve

--- ArraySolveContest3.1/F_FrontEnd.cpp
+++ ArraySolveContest3.1/F_FrontEnd.cpp
@@ -22,16 +22,34 @@ const ld PI = acos(-1.0);
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void solve() {
-    // Your code here
+void solve(int n) {
+    deque<int> a;
+    for (int i = 0; i < n; ++i) {
+        int num;
+        cin >> num;
+        a.push_back(num);
+    }
+
+    while (!a.empty()) {
+        cout << a.front() << " ";
+        a.pop_front();
+
+        if (!a.empty()) {
+            cout << a.back() << " ";
+            a.pop_back();
+        }
+    }
+
+    cout << endl;
+
+
 }
 
 int main() {
     fast_io;
-    int t = 1;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    int n;
+    cin >> n;
+    solve(n);
+    
     return 0;
 }
