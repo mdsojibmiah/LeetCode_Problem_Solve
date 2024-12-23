039d4793e70a6771d705d7054db061446b01dc95
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-23 20:53:57 +0600

    gbg

--- String_solve/B.cpp
+++ String_solve/B.cpp
@@ -13,12 +13,12 @@ using namespace std;
 #define ld long double
 #define pb push_back
 #define all(v) v.begin(), v.end()
-#define F first
-#define S second
+// #define F first
+// #define S second
 
-const ll INF = 1e18;
-const ll MOD = 1e9 + 7;
-const ld PI = acos(-1.0);
+// const ll INF = 1e18;
+// const ll MOD = 1e9 + 7;
+// const ld PI = acos(-1.0);
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
