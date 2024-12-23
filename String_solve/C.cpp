937a9afc4b165df5b6913ba5f30915becc50b9b9
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-24 03:17:38 +0600

    solve this proble

--- String_solve/C.cpp
+++ String_solve/C.cpp
@@ -2,7 +2,7 @@
    Email: mdsojibmiah.coder@gmail.com
    Date  : 12 / 24 / 2024
    Time  : 03:14
-   Problem Link: 
+   Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/C
 */
 
 #include <bits/stdc++.h>
@@ -14,22 +14,20 @@ using namespace std;
 #define pb push_back
 #define all(v) v.begin(), v.end()
 
-const ll INF = 1e18;
-const ll MOD = 1e9 + 7;
-const ld PI = acos(-1.0);
+// const ll INF = 1e18;
+// const ll MOD = 1e9 + 7;
+// const ld PI = acos(-1.0);
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 void solve() {
-    // Your code here
+    string X, Y;
+    cin >> X >> Y;
+    cout<<((X<Y)?X:Y)<<nl;
 }
 
 int main() {
     fast_io;
-    int t
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    solve();
     return 0;
 }
