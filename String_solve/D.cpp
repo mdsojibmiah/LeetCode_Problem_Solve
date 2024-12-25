ac15a0353748b5e90990c82b004d7bb3be05d50e
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-25 10:31:13 +0600

    solve

--- String_solve/D.cpp
+++ String_solve/D.cpp
@@ -20,16 +20,20 @@ const ld PI = acos(-1.0);
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void solve() {
-    // Your code here
-}
+// void solve() {
+//     // Your code here
+// }
 
 int main() {
     fast_io;
-    int t
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    string A, B;
+    cin >> A >> B;
+    int Alen = A.size();
+    int Blen = B.size();
+    cout << Alen << " " << Blen << nl;
+    cout << A << B << nl;
+    swap(A[0],B[0]);
+    cout << A << " " << B << nl;
+
     return 0;
 }
---
+++ ducational Codeforces Round 173/D.cpp
@@ -0,0 +1,6 @@
+
+    // if (!A.empty()&&!B.empty()) {
+    //     char temp = A[0];
+    //     A[0] = B[0];
+    //     B[0] = temp;
+    // }
