134ab70f1d06467293700eff57da939892b86243
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-25 10:38:50 +0600

    solved

--- String_solve/E.cpp
+++ String_solve/E.cpp
@@ -14,15 +14,15 @@ using namespace std;
 #define pb push_back
 #define all(v) v.begin(), v.end()
 
-const ll INF = 1e18;
-const ll MOD = 1e9 + 7;
-const ld PI = acos(-1.0);
+// const ll INF = 1e18;
+// const ll MOD = 1e9 + 7;
+// const ld PI = acos(-1.0);
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void solve() {
-    // Your code here
-}
+// void solve() {
+//     // Your code here
+// }
 
 int main() {
     fast_io;
