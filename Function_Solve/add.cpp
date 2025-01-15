444ec460308423d1134334e50928051b97bacd76
MD. SAJIB MIAH <89442408+mdsojibmiah@users.noreply.github.com>
2025-01-15 13:44:08 +0600

    Add prolblem solve

---
+++ Function_Solve/add.cpp
@@ -0,0 +1,33 @@
+/* Author: CodeWithError
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 01 / 15 / 2025
+   Time  : 08:36
+   Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/A
+*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define nl '\n'
+#define YES cout << "YES\n"
+#define Yes cout << "Yes\n"
+#define NO cout << "NO\n"
+#define No cout << "No\n"
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+ 
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    cout << x + y << nl;
+}
+ 
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
