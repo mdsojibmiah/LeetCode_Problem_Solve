26ea019a4a1087fdc60f312d45dd2cfbc676d361
MD. SAJIB MIAH <89442408+mdsojibmiah@users.noreply.github.com>
2025-01-27 21:07:45 +0600

    Update arrayfile.cpp

--- Function_Solve/arrayfile.cpp
+++ Function_Solve/arrayfile.cpp
@@ -1,1 +1,27 @@
-#temp 
+/* Author: CodeWithError
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 01 / 27 / 2025
+   Time  : 08:30 PM
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+    int n;
+    cin >> n;
+    set<int> s;
+    for (int i = 0, x; i < n; i++) {
+        cin >> x;
+        s.insert(x);
+    }
+    cout << s.size() << "\n";
+}
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
