5bcc0f52b5beca6c48670a6226c7f44dc9d9f44b
MD. SAJIB MIAH <89442408+mdsojibmiah@users.noreply.github.com>
2025-01-30 23:08:08 +0600

    Update temp.cpp

--- Function_Solve/temp.cpp
+++ Function_Solve/temp.cpp
@@ -1,1 +1,27 @@
-#temp
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap_rc(vector<vector<int>> &a, int x, int y) {
+    swap(a[x - 1], a[y - 1]);
+    for (int i = 0; i < a.size(); i++)
+        swap(a[i][x - 1], a[i][y - 1]);
+}
+
+int main() {
+    int n, x, y;
+    cin >> n >> x >> y;
+    vector<vector<int>> a(n, vector<int>(n));
+    
+    for (auto &r : a)
+        for (auto &c : r)
+            cin >> c;
+
+    swap_rc(a, x, y);
+
+    for (auto &r : a) {
+        for (auto &c : r)
+            cout << c << " ";
+        cout << endl;
+    }
+    return 0;
+}
